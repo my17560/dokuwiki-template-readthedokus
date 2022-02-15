a741a04e4f69e93fdaf33435b0169957629f3af1
Masaki Yasutake <my17560@gmail.com>
2022-02-15 19:26:55 +0900

    Added Japanese localization.

---
+++ lang/ja/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang["sidebarposition"] = "Sidebarの位置を指定します。";
+$lang["dokuwikibreadcrumbs_enable"] = "Dokuwikiのパンくずを使う場合、オンにします。";
+$lang["startpage"] = "最初のページのURLを指定します。";
+$lang["fontawesome_enable"] = "Font Awesomeを使う場合オンにします。下のオプションも忘れずにセットしてください。";
+$lang["fontawesome_tag"] = "Font Awesomeを使うためのタグを指定します。上のオプションにチェックを入れることを忘れないでください。注意:タグ全体を記述してください。例）&lt;link href='/css/all.css' rel='stylesheet'&gt;";
+$lang["googlefont_enable"] = "Google Fontsを使う場合オンにします。下のオプションも忘れずにセットしてください。";
+$lang["googlefont_tag"] = "Google Fontsを使うためのタグを指定します。上のオプションにチェックを入れることを忘れないでください。注意:タグ全体を記述してください。例）&lt;link rel='preload' href='/fonts/notosans.woff2' as='font' type='font/woff2' crossorigin&gt;";
