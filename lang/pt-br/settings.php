c79cc8fb058acebbd843b8778dfa16f39478660a
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2024-01-29 13:35:40 -0300

    Update settings.php

--- lang/pt-br/settings.php
+++ lang/pt-br/settings.php
@@ -5,6 +5,6 @@ $lang["dokuwikibreadcrumbs_enable"] = "Usar breadcrumbs do Dokuwiki";
 $lang["showtoolboxalways_enable"] = "Exibir ferramentas mesmo quando não autenticado";
 $lang["startpage"] = "Página inicial";
 $lang["fontawesome_enable"] = "Habilitar Font Awesome. Não se esqueça de definir uma tag correta no campo abaixo";
-$lang["fontawesome_tag"] = "Especifique uma tag para inclusão o Font Awesome. Não se esqueça de habilitar a caixa de seleção acima. Nota: especifique a tag completa. E.x. &lt;link href='/css/all.css' rel='stylesheet'&gt;";
+$lang["fontawesome_tag"] = "Especifique uma tag para inclusão do Font Awesome. Não se esqueça de habilitar a caixa de seleção acima. Nota: especifique a tag completa. E.x. &lt;link href='/css/all.css' rel='stylesheet'&gt;";
 $lang["googlefont_enable"] = "Habilitar Google Fonts";
 $lang["googlefont_tag"] = "Especifique tags para inclusão do Google Fonts. Não se esqueça de marcar a caixa de seleção acima. Nota: especifique a tag completa. E.x. &lt;link rel='preload' href='/fonts/notosans.woff2' as='font' type='font/woff2' crossorigin&gt;";
