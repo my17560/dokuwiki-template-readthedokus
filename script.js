582fa55c3cff37a9a5d77bd9394cd997790f7a86
Masaki Yasutake <my17560@gmail.com>
2024-04-27 14:14:18 +0900

    Removed IndexMenu2 workaround.

--- script.js
+++ script.js
@@ -9,8 +9,6 @@ document.addEventListener("DOMContentLoaded", function() {
 		var selector =".aside > #sidebar .indexmenu_js2 a[data-wiki-id]";
 		jQuery(".indexmenu_js2").on("fancytreeinit", (e) =>{
 			dokus.run({"linkSelector":selector});
-			// Workaround
-			setTimeout(() => document.styleSheets.item(0).insertRule("#dokuwiki__aside #sidebar .indexmenu_js2 ul.fancytree-container li {white-space:nowrap}", 0));
 		});
 		jQuery(".indexmenu_js2").on("fancytreeexpand", (e) =>{
 			dokus.refreshPageLinks(selector);
