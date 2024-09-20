83b5c91ea297cb45ab08aed7dc1a5609872b043c
Krzysztof Klimczak <klimczak.k@gmail.com>
2024-09-20 12:15:50 +0200

    Add polish settings.php

---
+++ lang/pl/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang["sidebarposition"] = "Pozycja paska bocznego";
+$lang["dokuwikibreadcrumbs_enable"] = "Użyj breadcrumbs Dokuwiki";
+$lang["showtoolboxalways_enable"] = "Pokaż pasek narzędzi, nawet gdy jesteś wylogowany";
+$lang["startpage"] = "Strona startowa";
+$lang["fontawesome_enable"] = "Włącz Font Awesome. Nie zapomnij ustawić poprawnego tagu w opcji poniżej";
+$lang["fontawesome_tag"] = "Określ tag by załączyć Font Awesome. <b>Nie zapomnij zaznaczyć opcji powyżej</b>. Informacja: podaj pełny tag, np.: &lt;link href='/css/all.css' rel='stylesheet'&gt;";
+$lang["googlefont_enable"] = "Włącz Google Fonts";
+$lang["googlefont_tag"] = "Określ tagi by załączyć Google Fonts. <b>Nie zapomnij zaznaczyć opcji powyżej</b>. Informacja: podawaj pełne tagi, np.: &lt;link rel='preload' href='/fonts/notosans.woff2' as='font' type='font/woff2' crossorigin&gt;";
